1e375cc8570eb77d162be675e1b61e5fd8a1cc76
btntazo <tamazimirianashvili@gmail.com>
2024-06-22 13:50:38 +0400

    refactor: Update CSS styles for ScrollableComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,54 @@
 import { Element } from 'react-scroll';
 import './App.css';
 import NavSection from './components/NavSection/NavSection';
+import ScrollableComponent from './components/ScrollableComponent/ScrollableComponent';
 
 function App() {
+
+  const project_screens = [
+    {
+      id: 1,
+      title: 'City Mall',
+      subtitle: 'City Mall is a shopping destination that brings people together from all walks of life.',
+      image: { src: 'assets/projects/slide-01.png', alt: 'city mall' },
+      description: [
+        'To help visitors find precisely what they are looking for, we doubled down on multiple arrows in the original symbol, updated and expanded them into a navigation-friendly design system.',
+        'Typography also got a bold upgrade integrating a custom Georgian adaptation of Pangram typeface.'
+      ],
+      style: { backgroundColor: '#fff' }
+    },
+    {
+      id: 2,
+      title: 'neyco',
+      subtitle: 'Out of nothing, a new source of wealth - Neyco shines through.',
+      image: { src: 'assets/projects/slide-02.png', alt: 'Neyco' },
+      description: [
+        'Whether it’s trading cryptocurrencies or pawning your bitcoin wallets to get fastaccess to cash.',
+        "Inspired by the eternal Creation of Adam our photo series for Neyco website tries to reframe the struggle between old and new forces, and help the viewers reimagine brand new source of wealth."
+      ],
+    },
+    {
+      id: 3,
+      title: 'KIND & NOBLE',
+      subtitle: 'Out of nothing, if you get tired chewing on your daily fruits and veggies, you can now drink them.',
+      image: { src: 'assets/projects/slide-03.png', alt: 'KIND & NOBLE' },
+      description: [
+        "«Kind and Noble» or [ke-ti-li da pa-ti-o-sa-ni] in our native Georgian language isn't just a name! This noble beverage is made exclusively by mixing fresh fruit and vegetable purees to bring mother nature's kindness to humans in the most convenient form."
+      ],
+    }
+  ];
+
+
   return (
     <>
       <Element name="section1">
         <NavSection />
       </Element>
       <Element name="section2">
-        <section style={{ height: '100vh', margin: 0, padding: 0, backgroundColor: '#000000' }}>
-          <h1>Section 2</h1>
-          <p>This is the content of section 2</p>
-        </section>
+        <ScrollableComponent screens={project_screens} />
       </Element>
       <Element name="section3">
-        <section style={{ height: '100vh', margin: 0, padding: 0, backgroundColor: '#FF0000' }}>
-          <h1>Section 3</h1>
-          <p>This is the content of section 2</p>
-        </section>
+        <ScrollableComponent screens={project_screens} />
       </Element>
       <Element name="section4">
         <section style={{ height: '100vh', margin: 0, padding: 0, backgroundColor: '#000000' }}>
--- src/components/ScrollableComponent/ScrollableComponent.jsx
+++ src/components/ScrollableComponent/ScrollableComponent.jsx
@@ -1,9 +1,62 @@
 import './ScrollableComponent.css';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-function ScrollableComponent() {
+function ScrollableComponent({ screens }) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handlePrevClick = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? screens.length - 1 : prevIndex - 1));
+    };
+
+    const handleNextClick = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === screens.length - 1 ? 0 : prevIndex + 1));
+    };
+
+    const currentScreen = screens[currentIndex];
+    const dotWidth = 100 / screens.length;
+
     return (
-        <div>ScrollableComponent</div>
+        <section style={{ backgroundColor: 'black', color: 'white' }}>
+            <div className="scrollable-component" >
+                <div className='screen'>
+                    <div className="header">
+                        <p>{currentScreen.subtitle}</p>
+                    </div>
+                    <div className="description">
+                        {currentScreen.description.map((desc, descIndex) => (
+                            <p key={descIndex} className='desc'>{desc}</p>
+                        ))}
+                    </div>
+                </div>
+                <div className="image-display">
+                    <div className="dots">
+                        {screens.map((_, index) => (
+                            <div
+                                key={index}
+                                className={`dot ${index === currentIndex ? 'active-dot' : ''}`}
+                                style={{ width: `${dotWidth}%` }}
+                                onClick={() => setCurrentIndex(index)}
+                            />
+                        ))}
+                    </div>
+                    <img src={currentScreen.image.src} alt={currentScreen.image.alt} className="image" />
+                    <div className='slide-num'>0{currentScreen.id}</div>
+                    <div className='nav-buttons'>
+                        <div className="nav-button prev" onClick={handlePrevClick}>‹</div>
+                        <div className="nav-button next" onClick={handleNextClick}>›</div>
+                    </div>
+                    <div className='title'>
+                        <h1>{currentScreen.title.toUpperCase()}</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
+
+ScrollableComponent.propTypes = {
+    screens: PropTypes.array.isRequired
+};
 
 export default ScrollableComponent
